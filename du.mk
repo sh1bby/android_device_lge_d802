1200042a48dcaa5020e1aedcd7eaf26c87c590c4
sh1bby <s.steffens87@gmail.com>
2016-03-12 19:52:07 +0100

    Setup for DU
    Change-Id: Id065f3d6695660925e8c159fe98ee2a26a6d1bb2

---
+++ du.mk
@@ -0,0 +1,20 @@
+# Inherit some common DU stuff.
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/du/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/d802/device.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := d802
+PRODUCT_NAME := du_d802
+PRODUCT_BRAND := LGE
+PRODUCT_MODEL := LG-D802
+PRODUCT_MANUFACTURER := lge
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=lge/g2_open_com/g2:5.0.2/LRX22G/151061918340a:user/release-keys \
+    PRIVATE_BUILD_DESC="g2_open_com-user 5.0.2 LRX22G 151061918340a release-keys"
